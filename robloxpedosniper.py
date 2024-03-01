60ecb3051d2c5b1977f9e4363068d239054da06d
epiclyrizzy <141612089+epiclyrizzy@users.noreply.github.com>
2024-02-29 17:17:01 -0800

    Update robloxpedosniper.py

--- robloxpedosniper.py
+++ robloxpedosniper.py
@@ -1,10 +1,20 @@
 import requests
+import configparser
 
-def get_user_profiles(group_ids):
+def read_config():
+    config = configparser.ConfigParser()
+    config.read('config.cfg')
+
+    group_ids = config['Settings']['group_ids'].split(',')
+    keywords = config['Settings']['keywords'].split(',')
+
+    return group_ids, keywords
+
+def get_user_profiles(group_ids, keywords):
     page_cursor = None
 
     for group_id in group_ids:
-        page_number = 1 
+        page_number = 1
         processed_groups = set()
 
         while True:
@@ -33,14 +43,16 @@ def get_user_profiles(group_ids):
                                 user_groups = groups_data.get("data", [])
 
                                 for group in user_groups:
-                                    group_id = group.get("group", {}).get("id")
+                                    group_info = group.get("group", {})
+                                    group_id = group_info.get("id")
+                                    group_name = group_info.get("name")
 
                                     
-                                    if group_id not in processed_groups:
+                                    if group_id not in processed_groups and any(keyword.lower() in group_name.lower() for keyword in keywords):
                                         processed_groups.add(group_id)
 
                                         group_link = f"https://www.roblox.com/groups/{group_id}/x"
-                                        print(f"User ID: {user_id}, Group ID: {group_id}, Group Link: {group_link}")
+                                        print(f"User ID: {user_id}, Group ID: {group_id}, Group Name: {group_name}, Group Link: {group_link}")
 
                             except requests.exceptions.HTTPError as http_err:
                                 print(f"Failed to retrieve groups for User ID: {user_id}. Status code: {groups_response.status_code}")
@@ -53,7 +65,7 @@ def get_user_profiles(group_ids):
                     if page_cursor is None:
                         break
 
-                    page_number += 1
+                    page_number += 1 
 
                 else:
                     print(f"Failed to retrieve data for Group ID: {group_id}. Status code: {response.status_code}")
@@ -64,5 +76,5 @@ def get_user_profiles(group_ids):
                 
 
 if __name__ == "__main__":
-    group_ids = ["33901499", "33687674"]
-    get_user_profiles(group_ids)
+    group_ids, keywords = read_config()
+    get_user_profiles(group_ids, keywords)
